13f37ee3d040b451b2cd55c16747db862405cbaf
David Shepherd <david@daveshep.net.nz>
2010-06-13 01:17:11 +0000

    work on importing toodledo
    git-svn-id: file:///var/svn-repos/list-manager/trunk@13 c44ac443-5e9e-4647-8042-5f20330fbc5f

--- src/main/java/daveshep/gtd/util/ToodledoXMLImporter.java
+++ src/main/java/daveshep/gtd/util/ToodledoXMLImporter.java
@@ -11,6 +11,9 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 import daveshep.gtd.ListManager;
+import daveshep.gtd.domain.ListItem;
+import daveshep.gtd.domain.Task;
+import daveshep.gtd.domain.TaskStatus;
 
 /**
  * @author david
@@ -53,10 +56,138 @@ public class ToodledoXMLImporter {
 			Node item;
 			for (int i=0;i<items.getLength();i++) {
 				item = items.item(i);
+
+				String itemId = "0";
+				String itemContext = "";
+				String itemTitle;
+				String itemStatus = "";
+				String itemFolder;
+				String itemParentId;
+				String itemCompletedDate;
+				String itemDueDate;
+				String itemGoal;
+				String itemTags;
+				String itemRepeat;
+				String itemStar;
+				String itemNotes;
 				
 				NodeList itemNodes = item.getChildNodes();
+				Node itemProperty;
+				for (int j=0;j<itemNodes.getLength();j++) {
+					itemProperty = itemNodes.item(j);
+					
+					if (itemProperty.getNodeType()!=Node.ELEMENT_NODE) {
+						continue;
+					}
+					
+					String itemPropertyName = itemProperty.getNodeName();
+					String itemPropertyValue = itemProperty.getNodeValue();
+
+					
+					if ("id".equals(itemPropertyName)) {
+						itemId = itemPropertyValue;
+					}
+
+					else if ("context".equals(itemPropertyName)) {
+						itemContext = itemPropertyValue;
+					}
+					
+					else if ("title".equals(itemPropertyName)) {
+						itemTitle = itemPropertyValue;
+					}
+					
+					else if ("folder".equals(itemPropertyName)) {
+						itemFolder = itemPropertyValue;
+					}
+					
+					else if ("status".equals(itemPropertyName)) {
+						itemStatus = itemPropertyValue;
+					}					
+
+					else if ("parent".equals(itemPropertyName)) {
+						itemParentId = itemPropertyValue;
+					}
+					
+					else if ("completed".equals(itemPropertyName)) {
+						itemCompletedDate = itemPropertyValue;
+					}
+					
+					else if ("duedate".equals(itemPropertyName)) {
+						itemDueDate = itemPropertyValue;
+					}
+
+					else if ("goal".equals(itemPropertyName)) {
+						itemGoal = itemPropertyValue;
+					}
+
+					else if ("tag".equals(itemPropertyName)) {
+						itemTags = itemPropertyValue;
+					}
+
+					else if ("repeat".equals(itemPropertyName)) {
+						itemRepeat = itemPropertyValue;
+					}
+
+					else if ("star".equals(itemPropertyName)) {
+						itemStar = itemPropertyValue;
+					}
 				
+					else if ("note".equals(itemPropertyName)) {
+						itemNotes = itemPropertyValue;
+					}
 				
+					// the remaining tags are ignored
+					else if ("startdate".equals(itemPropertyName)) {
+						continue;
+					}
+					
+					else if ("starttime".equals(itemPropertyName)) {
+						continue;
+					}
+					
+					else if ("duedatemodifier".equals(itemPropertyName)) {
+						continue;
+					}
+					
+					else if ("duetime".equals(itemPropertyName)) {
+						continue;
+					}
+					
+					else if ("priority".equals(itemPropertyName)) {
+						continue;
+					}
+					
+					else if ("length".equals(itemPropertyName)) {
+						continue;
+					}
+					
+					else if ("timer".equals(itemPropertyName)) {
+						continue;
+					}
+					
+					else {
+						// unknown tag
+						throw new Exception("item " + i + ": unknown tag " + j + ": " + itemPropertyName);
+					}
+				}
+				
+				// OK so now we have all the tag info, lets create our list item
+				ListItem listItem;
+				System.out.println(i + " " + itemStatus);
+				if (itemStatus.equals("Reference")) {
+					listItem = this.manager.createRefItem();
+				}
+				
+				else if (itemContext.equals("Projects")) {
+					listItem = this.manager.createProject();
+				}
+				
+				else {
+					listItem = this.manager.createTask();
+				}
+				
+				// and now populate the list item properties
+				listItem.setId(Long.valueOf(itemId));
 				
 			}
 			
