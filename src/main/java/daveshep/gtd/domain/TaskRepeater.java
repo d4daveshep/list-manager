ad334da5e4d38b4f0587f3cfbd53115c97dd5c8e
David Shepherd <david@daveshep.net.nz>
2010-06-27 05:01:46 +0000

    updated
    git-svn-id: file:///var/svn-repos/list-manager/trunk@17 c44ac443-5e9e-4647-8042-5f20330fbc5f

--- src/main/java/daveshep/gtd/domain/TaskRepeater.java
+++ src/main/java/daveshep/gtd/domain/TaskRepeater.java
@@ -1,5 +1,8 @@
 package daveshep.gtd.domain;
 
+import java.util.Calendar;
+import java.util.StringTokenizer;
+
 /**
  * Class to encapsulate the various ways to specify repeating tasks
  * @author shephd
@@ -11,8 +14,17 @@ public class TaskRepeater {
 		NONE, DAILY, WEEKLY, BI_WEEKLY, MONTHLY, BI_MONTHLY, QUARTERLY, HALF_YEARLY, YEARLY, SPECIAL  
 	}
 	
+	public enum RepeatInterval {
+		DAYS, WEEKS, MONTHS, YEARS  
+	}
+	
 	private RepeatType repeatType = RepeatType.NONE;
 	private String specialText;
+	private int repeatDayOfMonth;
+	private int repeatDayOfWeek;
+	private int repeatWeekOfMonth;
+	private RepeatInterval repeatInterval;
+	private int repeatMultiplier;
 	
 	public TaskRepeater() {
 	}
@@ -42,17 +54,64 @@ public class TaskRepeater {
 	 * accepts the form of
 	 * every <W> where W is <day of week|weekday|weekend> (e.g. every Monday)
 	 * every <X> <T> where T = <days|weeks|months|years> (e.g. every 2 days, every 8 weeks)
-	 * the <first|second|third|forth|last> <day of week> of every month (e.g. the second Monday of every month)
-	 *
+	 * the <N> <W> of every month where N = <first|second|third|forth|last> and W = <day of week> (e.g. the second Monday of every month)
+	 * the <D> of every month where D = <day of month> (e.g. the 20th of every month)
 	 * 
 	 * @param text
 	 * @throws IllegalArgumentException
 	 */
 	private void validate(String text) throws IllegalArgumentException {
 
-		if (false) {
+		StringTokenizer tokens = new StringTokenizer(text);
+		
+		String token = tokens.nextToken(); // get first token
+		if (token.equalsIgnoreCase("every")) {
+			token = tokens.nextToken(); // get the second token
+			if (token.equalsIgnoreCase("monday")) {
+				this.repeatDayOfWeek = Calendar.MONDAY;
+			}
+			else if (token.equalsIgnoreCase("tuesday")) {
+				this.repeatDayOfWeek = Calendar.TUESDAY;
+			}
+			else if (token.equalsIgnoreCase("wednesday")) {
+				this.repeatDayOfWeek = Calendar.WEDNESDAY;
+			}
+			else if (token.equalsIgnoreCase("thursday")) {
+				this.repeatDayOfWeek = Calendar.THURSDAY;
+			}
+			else if (token.equalsIgnoreCase("friday")) {
+				this.repeatDayOfWeek = Calendar.FRIDAY;
+			}
+			else if (token.equalsIgnoreCase("saturday")) {
+				this.repeatDayOfWeek = Calendar.SATURDAY;
+			}
+			else if (token.equalsIgnoreCase("sunday")) {
+				this.repeatDayOfWeek = Calendar.SUNDAY;
+			}
+			else try{ 
+				this.repeatMultiplier = Integer.parseInt(token);
+				
+				token = tokens.nextToken(); // get the third token
+				if (token.equalsIgnoreCase("days")) {
+					this.repeatInterval = RepeatInterval.DAYS;
+				} else if (token.equalsIgnoreCase("weeks")) {
+					this.repeatInterval = RepeatInterval.WEEKS;
+				} else if (token.equalsIgnoreCase("months")) {
+					this.repeatInterval = RepeatInterval.MONTHS;
+				} else if (token.equalsIgnoreCase("years")) {
+					this.repeatInterval = RepeatInterval.YEARS;
+				} else {
+					throw new IllegalArgumentException("special repeat text fails validation");
+				}
+				
+			} catch (NumberFormatException e) {
+				throw new IllegalArgumentException("special repeat text fails validation");
+			}
+		}
+		else if (token.equalsIgnoreCase("the")) {
 			
 		}
+		
 		else {
 			throw new IllegalArgumentException("special repeat text fails validation");
 		}
