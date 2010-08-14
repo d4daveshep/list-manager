90411b923337eac43f97592687eba1983193c4af
David Shepherd <david@daveshep.net.nz>
2010-08-14 01:59:48 +0000

    gui work
    git-svn-id: file:///var/svn-repos/list-manager/trunk@36 c44ac443-5e9e-4647-8042-5f20330fbc5f

--- src/main/java/daveshep/gtd/ui/swing/DeleteAction.java
+++ src/main/java/daveshep/gtd/ui/swing/DeleteAction.java
@@ -7,6 +7,7 @@ import java.util.List;
 import javax.swing.AbstractAction;
 import javax.swing.DefaultListModel;
 import javax.swing.JFrame;
+import javax.swing.JList;
 import javax.swing.JOptionPane;
 
 import daveshep.gtd.domain.ListItem;
@@ -23,13 +24,16 @@ public class DeleteAction extends AbstractAction {
 	public void actionPerformed(ActionEvent event) {
 		System.out.print("Delete... ");
 
-		Object[] selectedItems = frame.getItemList().getSelectedValues();
+		JList itemList = frame.getItemList();
+		Object[] selectedItems = itemList.getSelectedValues();
+		int selectedIndex = itemList.getSelectedIndex();
+		
 		int option = JOptionPane.showConfirmDialog(frame, "Delete "+ selectedItems.length + " items?", "Delete", JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null);
 		System.out.println(option);
 		if (option!=0) {
 			return;
 		} else {
-			DefaultListModel model = (DefaultListModel) frame.getItemList().getModel();
+			DefaultListModel model = (DefaultListModel) itemList.getModel();
 			for (int i=0;i<selectedItems.length;i++) {
 				ListItem listItem = (ListItem)selectedItems[i];
 				model.removeElement(listItem);
@@ -38,6 +42,6 @@ public class DeleteAction extends AbstractAction {
 		}
 		System.out.println(selectedItems.length + " items");
 		
-		// TODO get list to update immediately
+		itemList.setSelectedIndex(selectedIndex);
 	}
 }
